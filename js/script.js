a26383e60758d1f1385b0db3d17d99c29ff807a8
ChristianPucci91 <pucci.christian91@gmail.com>
2020-10-19 15:18:44 +0200

    js code cleaning

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-// Chiedi all'utente il suo nome
+// Chiedi all'utente il suo nome,cognome,colore preferito e generiamo una pw
 
   var nome = prompt("Qual'è il tuo nome ?");
-
-  //testiamo il funzionamento sull'inspector con console.log
-  console.log(nome);
-
-// Chiedi all'utente il suo cognome
-
   var cognome = prompt("Qual'è il tuo cognome ?");
-  console.log(cognome);
-
-// Chiedi all'utente il suo colore preferito
-
   var colorePreferito = prompt("Qual'è il tuo colore preferito ?");
-  console.log(colorePreferito);
-
-// Password generata
-
   var passwordGenerata = nome + cognome + colorePreferito + "19";
+
+// testiamo il funzionamento sull'inspector con console.log
+
+  console.log(nome);
+  console.log(cognome);
+  console.log(colorePreferito);
+  console.log("La pw generata sarà", passwordGenerata );
 
 // Risposta Output pwgen
 
   document.getElementById('pwgen').innerHTML = "La tua password generata è: " + nome + cognome + colorePreferito + "19";
-
-  console.log("La pw generata sarà", passwordGenerata );
